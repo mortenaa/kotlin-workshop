8fd43e038da00f7061a6888acf6afeb09df19a30
Morten Nygaard Åsnes <morten.aasnes@gmail.com>
2019-05-14 10:03:29 +0200

    Added Collection exercises

--- src/main/kotlin/no/mortenaa/exercises/part2/02_Collections.kt
+++ src/main/kotlin/no/mortenaa/exercises/part2/02_Collections.kt
@@ -2,6 +2,8 @@ package no.mortenaa.exercises.part2
 
 import no.mortenaa.data.AppInfo
 import no.mortenaa.data.Category
+import no.mortenaa.data.ContentRating
+import no.mortenaa.data.Genre
 
 /**
  *
@@ -31,59 +33,75 @@ interface AppStats {
 
     companion object {
         fun getAppStatsService(appInfoList: List<AppInfo>): AppStats {
-            return AppStatsService(appInfoList)
-            //TODO("Return your implementation of the AppStats interface here")
+            TODO("Return your implementation of the AppStats interface here")
         }
     }
+
+    /**
+     * The number of apps with a rating != null
+     */
+    fun ratedApps(): Int
 
     /**
      * Calculate the average rating for all apps in the list (excluding apps where rating == null)
      */
     fun averageRating(): Double
 
+    /**
+     * The average rating for all apps in a single category
+     */
     fun averageRating(category: Category): Double
 
+    /**
+     * Return the most expensive app
+     */
+    fun mostExpensiveApp(): AppInfo
+
+    /**
+     * Return a list of categories, ordered by average rating for the category from highest to lowest
+     */
     fun categoriesOrderedByRating(): List<Category>
 
+    /**
+     * Return a list of categories and number of apps in the category as a list of pairs, ordered from most to least
+     */
     fun categoriesOrderedByNumberOfApps(): List<Pair<Category, Int>>
 
+    /**
+     * Return the average rating for a single Genre
+     */
+    fun averageRatingForGenre(genre: Genre): Double
+
+    /**
+     * Find the Genre with the highest average rating
+     */
+    fun highestRatedGenre(): Genre
+
+    /**
+     * Return your own implementation of the [CategoryStats] interface for the specified Category
+     */
     fun categoryStats(category: Category): CategoryStats
 
+    /**
+     * Separate apps into groups according to ContentRating, and return as a Map from ContentRating => List of Apps
+     */
+    fun groupByContentRating(): Map<ContentRating, List<AppInfo>>
 
+    /**
+     * Return a Map from ContentRating => Average Rating
+     */
+    fun averageRatingByContentRating(): Map<ContentRating, Double>
+
+    /**
+     * Return a list with all apps that satisfy the given criteria. Order the list by number of reviews for each app
+     */
+    fun findApps(minRating: Double, maxPrice: Double, category: Category? = null, genre: Genre? = null): List<AppInfo>
 }
 
 interface CategoryStats {
-
     val name: String
-
     val size: Int
-
     val averageRating: Double
-
     val averagePrice: Double
-
 }
 
-class AppStatsService(val apps: List<AppInfo>) : AppStats {
-    override fun categoryStats(category: Category): CategoryStats {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun averageRating(category: Category): Double {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun categoriesOrderedByRating(): List<Category> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun categoriesOrderedByNumberOfApps(): List<Pair<Category, Int>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun averageRating(): Double {
-        // TODO()
-        return apps.mapNotNull { it.rating }.sumByDouble { it.stars } / apps.count { it.rating != null }
-    }
-
-}
